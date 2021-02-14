07d13e0f25fb72f337142a4bba2ebd81c4286d50
Andrey Kolomoets <kolomoets.and@gmail.com>
2021-02-14 23:37:39 +0200

    mob

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Hmaburger & NavbarMobile
+$(document).ready(function() {
+	$('.hamburger').click(function() {
+
+		$mobileNav = $('.menu');
+        $mobileLink = $('.menu_links');
+
+		if ($(this).hasClass('open')) {
+			$mobileNav.css({"width": "0"});
+            $mobileNav.css({"opacity": "0"});
+            $mobileNav.css({"visibility": "hidden"});
+		}
+		else {
+			$mobileNav.css({"width": "80%"});
+            $mobileNav.css({"opacity": "1"});
+            $mobileNav.css({"visibility": "visible"});
+		}
+
+		$(this).toggleClass('open');
+	});
+});
